6bc4252bc34f779fc34552b8865d77b1f4677208
Manish Ratnawat <ratnawatmanish031@gmail.com>
2024-06-30 16:58:42 +0530

    fix bugs vercel config

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,19 @@ const app = express()
 const port = 5000;
 const connectToDatabase = require('./db');
 
-app.use(cors({
-  origin: 'https://vocab-quiz-frontend.vercel.app', 
-  methods: ['GET','PUT','POST','DELETE'],
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'https://vocab-quiz-frontend.vercel.app', 
+//   methods: ['GET','PUT','POST','DELETE'],
+//   credentials: true,
+// }));
 connectToDatabase();
 
 app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send("Hello Vercel");
 })
+
 app.use('/quiz', require('./route/queRouter'));
 app.use('/auth', require('./route/authRouter'));
 
