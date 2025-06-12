a6949decc73dfceb5f687b0218d2ae4a767399bb
manish <ratnawatmanish031@gmail.com>
2025-06-13 01:00:44 +0530

    add button and navbar fixed

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
           Vocabulary Quiz
